3153b13ecd3fb1b0790403ae0200c21dc0ce45b1
Kamil <kamilpier08@gmail.com>
2018-06-15 15:01:25 +0200

    fileList

--- src/app/create-note-group/create-note-group.component.ts
+++ src/app/create-note-group/create-note-group.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-note-group',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-note-group.component.scss']
 })
 export class CreateNoteGroupComponent implements OnInit {
-
-  constructor() { }
+  public stepp;
+  constructor(private router: Router) {
+    this.stepp = 1;
+   }
 
   ngOnInit() {
   }
+
+  nextStepp(){
+    this.stepp = 2;
+    document.getElementById('one').classList.remove('selected');
+    document.getElementById('two').classList.add('selected');
+    document.getElementById('sr').classList.add('second-stepp');
+  }
+  
+  prevStepp(){
+    this.stepp = 1;
+    document.getElementById('one').classList.add('selected');
+    document.getElementById('two').classList.remove('selected');
+    document.getElementById('sr').classList.remove('second-stepp');
+  }
+
+  createGroup(){
+    console.log("Zapisano")
+    this.router.navigate(['/home', {outlets: {'content': ['passwdList']}}]);
+  }
+
 
 }
